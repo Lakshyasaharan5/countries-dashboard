625c61ab8a4a5a4778853bf060ceab00f69dd48a
Lakshya Saharan <lakshyasaharan@gmail.com>
2026-07-05 01:04:46 -0400

    layer2: data driven markup

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,46 @@
+import { api_data, regions } from "./lib/data"
 
 function App() {
+    const countries = api_data.data.objects;
+    const totalCountries = countries.length;
+    let maxPopulation = 0;
+    let maxArea = 0 // miles
+    let maxPopulatedCountry = "";
+    let largestCountry = "";
+    for (let i = 0; i < totalCountries; i++) {
+        const currCountry = countries[i];
+        if (currCountry.population > maxPopulation) {
+            maxPopulation = currCountry.population;
+            maxPopulatedCountry = currCountry.names.common;
+        }
+        if (currCountry.area.miles > maxArea) {
+            maxArea = currCountry.area.miles;
+            largestCountry = currCountry.names.common;
+        }
+    }
     return (
         <div className="min-h-screen">
             <div className="max-w-5xl mx-auto px-4 flex flex-col gap-5">
                 <h1 className="text-2xl font-bold text-center">Countries data dashboard</h1>
                 <div className="flex gap-5 text-center">
-                    <div className="flex-1">total countries</div>
-                    <div className="flex-1">max population</div>
-                    <div className="flex-1">max area</div>
+                    <div className="flex-1">
+                        <h2>Total Countries</h2>
+                        <h4>{totalCountries}</h4>
+                    </div>
+                    <div className="flex-1">
+                        <h2>Most Populated Country</h2>
+                        <h4>{maxPopulatedCountry}</h4>
+                    </div>
+                    <div className="flex-1">
+                        <h2>Largest Country</h2>
+                        <h4>{largestCountry}</h4>
+                    </div>
                 </div>
                 <div className="flex gap-5 justify-center">
                     <input className="border rounded-sm" type="text" placeholder="Search country" />
                     <select className="border rounded-sm">
-                        <option value="">select region</option>
-                        <option value="europe">europe</option>
-                        <option value="asia">asia</option>
+                        <option value="">Select region</option>
+                        {regions.map((value) => <option key={value} value={value}>{value}</option>)}
                     </select>
                 </div>
                 <table className="text-center">
@@ -23,28 +49,24 @@ function App() {
                             <th>Country</th>
                             <th>Capital</th>
                             <th>Region</th>
-                            <th>Area</th>
+                            <th>Area (miles)</th>
                             <th>Population</th>
-                            <th>Density</th>
+                            <th>Density (pop/area)</th>
                         </tr>
                     </thead>
                     <tbody>
-                        <tr>
-                            <td>India</td>
-                            <td>New Delhi</td>
-                            <td>Asia</td>
-                            <td>3287263</td>
-                            <td>1400000000</td>
-                            <td>425</td>
-                        </tr>
-                        <tr>
-                            <td>France</td>
-                            <td>Paris</td>
-                            <td>Europe</td>
-                            <td>551695</td>
-                            <td>68000000</td>
-                            <td>123</td>
-                        </tr>
+                        {
+                            countries.map((value) =>
+                                <tr key={value.names.common}>
+                                    <td>{value.names.common}</td>
+                                    <td>{value.capitals[0]?.name ?? "-"}</td>
+                                    <td>{value.region}</td>
+                                    <td>{value.area.miles}</td>
+                                    <td>{value.population}</td>
+                                    <td>{(value.population / value.area.miles).toFixed(2)}</td>
+                                </tr>
+                            )
+                        }
                     </tbody>
                 </table>
             </div>
---
+++ src/lib/data.ts
@@ -0,0 +1,86 @@
+export const api_data = {
+    "data": {
+        "objects": [
+            {
+                "names": {
+                    "common": "Abkhazia"
+                },
+                "capitals": [
+                    {
+                        "attributes": {
+                            "administrative": false,
+                            "constitutional": false,
+                            "executive": false,
+                            "judicial": false,
+                            "legislative": false,
+                            "primary": true
+                        },
+                        "coordinates": {
+                            "lat": 43.0033,
+                            "lng": 41.0153
+                        },
+                        "name": "Sukhumi"
+                    }
+                ],
+                "region": "Asia",
+                "area": {
+                    "kilometers": 8665,
+                    "miles": 3345.6
+                },
+                "population": 244000,
+                "_meta": {
+                    "lastUpdatedTimestamp": 1783005246
+                }
+            },
+            {
+                "names": {
+                    "common": "Afghanistan"
+                },
+                "capitals": [
+                    {
+                        "attributes": {
+                            "administrative": false,
+                            "constitutional": false,
+                            "executive": false,
+                            "judicial": false,
+                            "legislative": false,
+                            "primary": true
+                        },
+                        "coordinates": {
+                            "lat": 34.52,
+                            "lng": 69.18
+                        },
+                        "name": "Kabul"
+                    }
+                ],
+                "region": "Asia",
+                "area": {
+                    "kilometers": 652230,
+                    "miles": 251827.3
+                },
+                "population": 35000000,
+                "_meta": {
+                    "lastUpdatedTimestamp": 1783005246
+                }
+            }
+        ],
+        "meta": {
+            "total": 254,
+            "count": 2,
+            "limit": 2,
+            "offset": 0,
+            "more": true,
+            "request_id": "008f194a-9790-4390-807d-2426a71926b6",
+            "duration": 25
+        }
+    }
+}
+
+export const regions = [
+    "Africa",
+    "Americas",
+    "Asia",
+    "Europe",
+    "Oceania",
+    "Antarctic",
+];
